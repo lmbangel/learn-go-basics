c048c92955302b251610363c4c5d60cd9243e95d
lmbangel <mbangelalihle@gmail.com>
2023-03-20 05:20:58 +0200

    hangle json

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
-"log"
+	"log"
+	"encoding/json"
+	// "github.com/gorilla/mux"
 )
 
 type User struct{
-
+	Firstname 		string `json:"firstname"`
+	Lastname 		string `json:"lastname"`
+	Email 			string `json:"email"`
+	Cellnumber 		string `json:"cellnumber"`
+	ID 				int `json:"id"`
 }
-type Account struct{
 
-}
+// type Account struct{
+
+// }
 
 func handleGetUser(w http.ResponseWriter, r *http.Request){
 	if r.URL.Path != "/user" {
@@ -22,16 +29,36 @@ func handleGetUser(w http.ResponseWriter, r *http.Request){
 		http.Error(w, "Request method not supported.", 404)
         return
 	}
-	fmt.Fprintf(w, "handling get user function.")
+
+	data := User {Firstname: "Lihle", Lastname : "Mbangela", Email : "mbangelalihle@gmail.com", Cellnumber : "+27672966361", ID: 1,}
+	
+	userData, error := json.Marshal(data);
+	if error != nil {
+		http.Error(w, error.Error(), 500)
+        return
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+	// json.NewEncoder(w).Encode(userData)
+	
+	// json.NewEncoder(w).Encode(userData)
+	// w.Header().Set("Content-Type", "application/json")
+	w.Write(userData)
+	return
 }
 
 func main(){
 	//listen here;
+	// router := mux.NewRouter()
+
+
 	serveLandingPage := http.FileServer(http.Dir("./public"))
 	http.Handle("/",serveLandingPage)
+	// router.Handle("/", serveLandingPage).Methods("GET")
+	// http.HandleFunc("/user", handleGetUser)
+
 	http.HandleFunc("/user", handleGetUser)
 
-	
     fmt.Println("Listening on port 8080")
 	if error := http.ListenAndServe(":8080", nil);
 	error != nil {
